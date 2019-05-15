846d5ffb6adc9bdb6cf35dcf1ceed1b8f1e903a6
Thiago Moreira <39631037+thiagomr8@users.noreply.github.com>
2019-05-14 23:26:08 -0300

    Agora condiz com os prints

--- arquivos/select_where.sql
+++ arquivos/select_where.sql
@@ -1,8 +1,8 @@
 /*9.2	CONSULTAS DAS TABELAS COM FILTROS WHERE (Mínimo 4)*/
-SELECT cod_lixeira, cod_bairro, capacidade FROM lixeira WHERE capacidade > 100;
-SELECT nome FROM cidadao WHERE email = 'maria123@gmail.com';
-SELECT cod_bairro, nome FROM bairro WHERE cod_bairro = '4';
-SELECT cod_lixeira FROM situacao_operacional WHERE cod_status = 1;
-SELECT cod_coletor, data_coleta FROM coleta WHERE data_coleta > '2019-03-24';
-SELECT placa, modelo, marca FROM coletor WHERE ano = 2019;
-SELECT volume, hora_descarte FROM descarte WHERE hora_descarte < '17:00:00';
+SELECT * FROM lixeira WHERE capacidade > 100;
+SELECT * FROM cidadao WHERE email = 'maria123@gmail.com';
+SELECT * FROM bairro WHERE cod_bairro = '4';
+SELECT * FROM situacao_operacional WHERE cod_status = 1;
+SELECT * FROM coleta WHERE data_coleta > '2019-03-24';
+SELECT * FROM coletor WHERE ano = 2019;
+SELECT * FROM descarte WHERE hora_descarte < '17:00:00';
