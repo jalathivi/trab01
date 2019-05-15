106a2f2f491ddb574f878262b30ac3519b30c77e
Thiago Moreira <39631037+thiagomr8@users.noreply.github.com>
2019-05-14 21:48:46 -0300

    Era nível, agora volume

--- arquivos/select_where.sql
+++ arquivos/select_where.sql
@@ -5,4 +5,4 @@ SELECT cod_bairro, nome FROM bairro WHERE cod_bairro = '4';
 SELECT cod_lixeira FROM situacao_operacional WHERE cod_status = 1;
 SELECT cod_coletor, data_coleta FROM coleta WHERE data_coleta > '2019-03-24';
 SELECT placa, modelo, marca FROM coletor WHERE ano = 2019;
-SELECT nivel, hora_descarte FROM descarte WHERE hora_descarte < '17:00:00';
+SELECT volume, hora_descarte FROM descarte WHERE hora_descarte < '17:00:00';
