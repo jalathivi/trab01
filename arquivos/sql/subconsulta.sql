8c9ab96419b7317a96a79533b976e1811ce47aab
Jackson Willian <jacksonwilliansilvaagostinho@gmail.com>
2019-06-09 14:27:56 -0300

    Atualiza consulta 9.10

--- arquivos/sql/subconsulta.sql
+++ arquivos/sql/subconsulta.sql
@@ -1,5 +1,7 @@
-/*  Os modelo de caminhão coletor diferente de VEGALIX */
-select distinct coletor.modelo as modelo_de_caminhao from coletor where modelo in (select distinct coletor.modelo from coletor where modelo <> 'VEGALIX')
+/*  Os modelo de caminhão coletor diferente de 'VW 8150' */
+SELECT DISTINCT nome as modelo_de_caminhao FROM modelo WHERE nome IN (SELECT DISTINCT nome FROM modelo WHERE nome <> 'VW 8150')
+
+
 
 /* Código das lixeiras que possuem o nível atual de lixo acima da média dos níveis atuais de todas lixeiras*/
 select lixeira.cod_lixeira as codigo, lixeira.nivel_atual from lixeira where nivel_atual > (select avg(nivel_atual) from lixeira)
