511864eaf8641165f111dc035c14b1fe5bd5205e
Jackson Willian <jacksonwilliansilvaagostinho@gmail.com>
2019-06-10 18:19:30 -0300

    Update 9_8_right_left_join.sql

--- arquivos/sql/9_8_right_left_join.sql
+++ arquivos/sql/9_8_right_left_join.sql
@@ -1,2 +1,5 @@
 --Seleciona lixeiras, seus bairros e sua capacidade
 SELECT cod_lixeira, bairro.cod_bairro, bairro.nome, capacidade FROM bairro LEFT OUTER JOIN lixeira ON (lixeira.cod_bairro = bairro.cod_bairro);
+
+--Seleciona caminhão, modelo e modelo
+
